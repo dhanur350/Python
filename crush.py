1e2c8fffbee98410d5faa92d91a0fee5a47db5e4
dhanur350 <dhanur297@gmail.com>
2021-08-06 23:58:05 +0530

    Day1

--- crush.py
+++
@@ -1,8 +0,0 @@
-a="learn" 
-b="about"
-c="string"
-d="variable"
-e="in"
-f="python"
-res=c[3]+a[0]+b[2]+d[0]+d[len(d)-1]+b[-2]
-print(res)
--- list.py
+++ list.py
@@ -6,3 +6,4 @@ This is a temporary script file.
 """
 import matplotlib.pyplot as plt
 plt.plot([1,2,3,4],[1,4,9,16])
+plt.show()
